2c735290bb0a1dfb64f3a46c0bc11b4431d91de0
jmcmonagleDCSD <120748576+jmcmonagleDCSD@users.noreply.github.com>
2024-12-04 09:04:55 -0700

    Update terrian and spawns

--- Scenes/Manager/enemy_manager.gd
+++ Scenes/Manager/enemy_manager.gd
@@ -14,6 +14,26 @@ func _ready() -> void:
 	timer.timeout.connect(on_timer_timeout)
 	arena_time_manager.arena_difficulty_increased.connect(on_arena_difficulty_increased)
 	
+
+func get_spawn_position() -> Vector2:
+	var player = get_tree().get_first_node_in_group("player") as Node2D
+	if player == null:
+		return Vector2.ZERO
+		
+	var spawn_position = Vector2.ZERO
+	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
+	for i in 4:
+		spawn_position = player.global_position + (random_direction * SPAWN_RADIUS)
+
+		var query_parameters = PhysicsRayQueryParameters2D.create(player.global_position, spawn_position, 1)
+		var result = get_tree().root.world_2d.direct_space_state.intersect_ray(query_parameters)
+		if result.is_empty(): 
+			break
+		else:
+			random_direction = random_direction.rotated(deg_to_rad(90))
+		
+	return spawn_position
+	
 	
 func on_timer_timeout() -> void:
 	timer.start()
@@ -22,17 +42,15 @@ func on_timer_timeout() -> void:
 	if player == null:
 		return
 	
-	var random_direction = Vector2.RIGHT.rotated(randf_range(0, TAU))
-	var spwan_position = player.global_position + (random_direction * SPAWN_RADIUS)
 	var enemy_instance = basic_enemy_scene.instantiate() as Node2D
 	
 	var entites_layer = get_tree().get_first_node_in_group("entities_layer")
 	entites_layer.add_child(enemy_instance)
-	enemy_instance.global_position = spwan_position
+	enemy_instance.global_position = get_spawn_position()
 
 
 func on_arena_difficulty_increased(arena_difficulty: int) -> void:
 	var time_off = (.1/12) * arena_difficulty
 	time_off = min(time_off, .7)
 	timer.wait_time = base_spawn_time - time_off
-	print(time_off)
+	
